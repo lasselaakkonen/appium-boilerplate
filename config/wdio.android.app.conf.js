c71c61d93f33a980f090a35802390704b3061e78
Lasse Laakkonen <lasse@laakkonen.net>
2020-12-03 12:54:05 +0200

    Change wdio.android.app.conf to use MultiRemote Capabilities, so that we can access both a browser and a mobile app at the same time.

--- config/wdio.android.app.conf.js
+++ config/wdio.android.app.conf.js
@@ -13,27 +13,70 @@ config.specs = [
 // ============
 // For all capabilities please check
 // http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
-config.capabilities = [
-    {
-        // The defaults you need to have in your config
-        platformName: 'Android',
-        maxInstances: 1,
-        // For W3C the appium capabilities need to have an extension prefix
-        // http://appium.io/docs/en/writing-running-appium/caps/
-        // This is `appium:` for all Appium Capabilities which can be found here
-        'appium:deviceName': 'Pixel_8.1',
-        'appium:platformVersion': '8.1',
-        'appium:orientation': 'PORTRAIT',
-        // `automationName` will be mandatory, see
-        // https://github.com/appium/appium/releases/tag/v1.13.0
-        'appium:automationName': 'UiAutomator2',
-        // The path to the app
-        'appium:app': join(process.cwd(), './apps/Android-NativeDemoApp-0.2.1.apk'),
-        // Read the reset strategies very well, they differ per platform, see
-        // http://appium.io/docs/en/writing-running-appium/other/reset-strategies/
-        'appium:noReset': true,
-        'appium:newCommandTimeout': 240,
+
+// config.capabilities = [
+//     {
+//         // The defaults you need to have in your config
+//         platformName: 'Android',
+//         maxInstances: 1,
+//         // For W3C the appium capabilities need to have an extension prefix
+//         // http://appium.io/docs/en/writing-running-appium/caps/
+//         // This is `appium:` for all Appium Capabilities which can be found here
+//         // 'appium:deviceName': 'Pixel_8.1',
+//         // 'appium:platformVersion': '8.1',
+//         // 'appium:orientation': 'PORTRAIT',
+//         // `automationName` will be mandatory, see
+//         // https://github.com/appium/appium/releases/tag/v1.13.0
+//         'appium:automationName': 'UiAutomator2',
+//         // The path to the app
+//         'appium:app': join(process.cwd(), './apps/Android-NativeDemoApp-0.2.1.apk'),
+//         // Read the reset strategies very well, they differ per platform, see
+//         // http://appium.io/docs/en/writing-running-appium/other/reset-strategies/
+//         'appium:noReset': true,
+//         'appium:newCommandTimeout': 240,
+//     },
+// ];
+
+config.capabilities = {
+    android: {
+        host: '127.0.0.1',
+        port: 4723,
+        capabilities: {
+            // The defaults you need to have in your config
+            platformName: 'Android',
+            maxInstances: 1,
+            // For W3C the appium capabilities need to have an extension prefix
+            // http://appium.io/docs/en/writing-running-appium/caps/
+            // This is `appium:` for all Appium Capabilities which can be found here
+            // 'appium:deviceName': 'Pixel_8.1',
+            // 'appium:platformVersion': '8.1',
+            // 'appium:orientation': 'PORTRAIT',
+            // `automationName` will be mandatory, see
+            // https://github.com/appium/appium/releases/tag/v1.13.0
+            'appium:automationName': 'UiAutomator2',
+            // The path to the app
+            'appium:app': join(process.cwd(), './apps/Android-NativeDemoApp-0.2.1.apk'),
+            // Read the reset strategies very well, they differ per platform, see
+            // http://appium.io/docs/en/writing-running-appium/other/reset-strategies/
+            'appium:noReset': true,
+            'appium:newCommandTimeout': 240,
+        },
     },
-];
+    chrome: {
+        host: '127.0.0.1',
+        port: 9515,
+        // port: 4444,
+        // browserName: 'chrome',
+        capabilities: {
+            // maxInstances: 1,
+            browserName: 'chrome',
+            'goog:chromeOptions': {
+            // args: (DEBUG ?
+            //     ['--window-size=1440,900', '--remote-debugging-port=19222'] :
+            //     ['--window-size=1440,900', '--headless'])
+            }
+        },
+    },
+};
 
 exports.config = config;
